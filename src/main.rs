bee7f32c43d239a0c6df78b01daae5bcbaeeb628
Kai <thiskaiguy@gmail.com>
2020-03-26 09:06:10 +0000

    Create an actix-web server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use {
+    std::env,
+    actix_web::{App, HttpServer},
+};
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(move || {
+        App::new()
+    })
+    .bind(("0.0.0.0", server_port()))?
+    .run()
+    .await
+}
+
+fn server_port() -> u16 {
+    env::var("PORT")
+        .map(|port| {
+            port.parse()
+                .expect("PORT must be a number")
+        })
+        .unwrap_or(3000)
 }
